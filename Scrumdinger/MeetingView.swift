59851597b576d143f7b27c1489afd5fccc120f8d
Ghaffaru Mudashiru <mudashiruagm@gmail.com>
2023-06-16 18:07:27 +0000

    add accessibility controls

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -12,16 +12,33 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack (alignment: .leading){
                     Text("Seconds Elapsed")
+                        .font(.caption)
+                        
                     Label("300",systemImage: "hourglass.tophalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack (alignment: .trailing){
                     Text("Seconds Remaining")
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button (action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+            .accessibilityLabel("Next speaker")
         }
+        .padding()
     }
 }
 
