24a43473ce900a9d135475a92aa660f4004fae2f
Ghaffaru Mudashiru <mudashiruagm@gmail.com>
2023-06-16 17:33:45 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ghaffaru Mudashiru on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
