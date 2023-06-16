e3bfc586823ba2d318c4f292764c95944c273bfd
Ghaffaru Mudashiru <mudashiruagm@gmail.com>
2023-06-16 17:42:35 +0000

    start meeting view

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Ghaffaru Mudashiru on 16/06/2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300",systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
